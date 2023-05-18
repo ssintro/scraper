7ee7cf400d52ce1c8fc28d5da53bd6f6687f1e41
ssintro <levkleshchenko@gmail.com>
2023-05-18 14:36:34 +0200

    change project structure

--- main.py
+++ main.py
@@ -4,44 +4,37 @@ from bs4 import BeautifulSoup
 import requests
 import lxml
 
-headers = {
-    "accept": "*/*",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36 Edg/113.0.1774.42"
-}
+URL = 'https://dou.ua/'
 
-URL_PRICE = 'https://dashboard.pnmtoken.com/api/dpnm/price'
-URL = 'https://dpnmdefi.com/app/'
+logging.basicConfig(level=logging.DEBUG,
+                    filename="logs.log",
+                    format="%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
+                    datefmt='%H:%M:%S')
 
-def logs():
-    logging.basicConfig(
-    level=logging.DEBUG,
-    filename = "requests.log",
-    format = "%(asctime)s - %(module)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s",
-    datefmt='%H:%M:%S',
-    )
+def getInfo():
 
-def getUserInfo():
-    responseInfo = requests.get(URL, headers=headers)
-    soup = BeautifulSoup(responseInfo.content, 'lxml')
-    data_balance = soup.find("div", class_="flex-grow-1 ms-3")
-    print(data_balance)
+    logging.info("Started")
 
-def getPrice():
+    try:
+        response = requests.get(URL)
+        soup = BeautifulSoup(response.text, 'lxml')
+        info = soup.find('div', class_='sc-9d68cce4-0.sc-9d68cce4-3.gPdXCK').find(
+        'tr')
 
-    responsePrice = requests.get(URL_PRICE, headers=headers)
-    responsePrice.raise_for_status()  # Raise an exception if the status code is an error code
-    rt = responsePrice.text
+        logging.debug("Processing")
 
-    rt_list = json.loads(rt)
-    result = [f"Date: {x['date']}, Price: {x['price']:.4f}\n" for x in rt_list]
+        logging.info("Success")
 
-    with open('data.txt', 'w') as file:
-        file.writelines(result)
+        logging.info("Ended")
+
+    except Exception as e:
+        logging.error('Error: %s', str(e))
+
+        logging.info("Ended")
 
 def main():
-    logs()
-    getPrice()
-    getUserInfo()
+    getInfo()
+
 
 if __name__ == '__main__':
     main()
